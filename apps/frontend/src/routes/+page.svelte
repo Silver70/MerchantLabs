1cb9f812312e29fc1a4f27009b781ccb550520e0
Silver70 <jsameeu@gmail.com>
2025-11-14 16:24:42 +0500

    frontend setup

---
+++ apps/frontend/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1 class="text-2xl font-bold text-blue-500">Welcome to SvelteKit</h1>
+<p class="text-xl font-semibold text-green-500">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
